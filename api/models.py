9bbb21df1945c654142c65f5f851e1bb70875f2e
LjuboM <ljubo.galcon@gmail.com>
2021-01-06 15:07:24 +0100

    Model User

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    age = models.IntegerField()
+    money = models.IntegerField()
+
+    def __str__(self):
+        return self.name
