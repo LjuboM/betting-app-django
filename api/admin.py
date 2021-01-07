d59570aee4a970de66bd181b0545d5613019ac3e
LjuboM <ljubo.galcon@gmail.com>
2021-01-07 23:23:07 +0100

    Added Ticket Odds TicketOdds to admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Transaction, Types, Match
+from .models import User, Transaction, Types, Match, Odds, Ticket, TicketOdds
 
-# Register your models here.
 admin.site.register(User)
 admin.site.register(Transaction)
 admin.site.register(Types)
 admin.site.register(Match)
+admin.site.register(Odds)
+admin.site.register(Ticket)
+admin.site.register(TicketOdds)
