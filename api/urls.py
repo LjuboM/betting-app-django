6c9f93ba85865fc415148c6be6939717dd42f579
LjuboM <ljubo.galcon@gmail.com>
2021-01-08 22:31:04 +0100

    Added empty frontend

--- api/urls.py
+++ api/urls.py
@@ -2,24 +2,24 @@ from django.urls import path
 from .views import UserView, UsersView, TransactionView, TransactionsView, TypesPerSportView, TypesView, MatchView, MatchesView, TicketView, TicketsView, OddsPerMatchView, OddsView, TicketOddsDetailsView, TicketOddsView
 
 urlpatterns = [
-    path('api/user/<int:id>/', UserView.as_view()),
-    path('api/user/', UsersView.as_view()),
+    path('user/<int:id>/', UserView.as_view()),
+    path('user/', UsersView.as_view()),
 
-    path('api/transaction/<int:id>/', TransactionView.as_view()),
-    path('api/transaction/', TransactionsView.as_view()),
+    path('transaction/<int:id>/', TransactionView.as_view()),
+    path('transaction/', TransactionsView.as_view()),
 
-    path('api/types/<int:id>/', TypesPerSportView.as_view()),
-    path('api/types/', TypesView.as_view()),
+    path('types/<int:id>/', TypesPerSportView.as_view()),
+    path('types/', TypesView.as_view()),
 
-    path('api/match/<int:id>/', MatchView.as_view()),
-    path('api/match/', MatchesView.as_view()),
+    path('match/<int:id>/', MatchView.as_view()),
+    path('match/', MatchesView.as_view()),
 
-    path('api/ticket/<int:id>/', TicketView.as_view()),
-    path('api/ticket/', TicketsView.as_view()),
+    path('ticket/<int:id>/', TicketView.as_view()),
+    path('ticket/', TicketsView.as_view()),
 
-    path('api/odds/<int:id>/', OddsPerMatchView.as_view()),
-    path('api/odds/', OddsView.as_view()),
+    path('odds/<int:id>/', OddsPerMatchView.as_view()),
+    path('odds/', OddsView.as_view()),
 
-    path('api/ticketOdds/<int:id>/', TicketOddsDetailsView.as_view()),
-    path('api/ticketOdds/', TicketOddsView.as_view())
+    path('ticketOdds/<int:id>/', TicketOddsDetailsView.as_view()),
+    path('ticketOdds/', TicketOddsView.as_view())
 ]
